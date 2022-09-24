94851a49ba0045f1ea466f7682019b22f4039431
abstractjazz <evan@heyevan.com>
2022-09-24 12:48:18 -0700

    refactors tailwind to external styles

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import indexStyles from './styles.js'
 
 export default function Home() {
   return (
-    <div className="">
+    <div className={indexStyles.container}>
       <Head>
         <title>All 7z</title>
-       
-      
       </Head>
-
+      <h1>YO!</h1>
     </div>
   )
 }
---
+++ pages/styles.js
@@ -0,0 +1,5 @@
+export default {
+    container:[ 
+        'bg-pink-500'
+    ].join(' ')
+}
