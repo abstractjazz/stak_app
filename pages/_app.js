a7600d2d4985cc07444792415298196a121c751c
abstractjazz <evan@heyevan.com>
2022-09-24 00:56:35 -0700

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+  <>
+      <Head>
+        <meta name="viewport" content="viewport-fit=cover" />
+        <meta name="description" content="All 7z: Music, Lifestyle, Merch" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="robots" content="index, follow"/>
+        <meta property="og:title" content="All 7z: Music, Lifestyle, Merch"/>
+        <meta property="og:site_name" content="All 7z Brand"/>
+        <meta property="og:description" content="Explore the All 7z Brand. West Coast Music, Lifestyle, Merch"/>
+        <meta property="twitter:title" content="All 7z: Music, Lifestyle, Merch"/>
+        <meta property="twitter:description" content="Explore the All 7z Brand. West Coast Music, Lifestyle, Merch"/>
+      </Head>
+      <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
